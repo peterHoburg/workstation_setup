85b78d32cc3de6ee59e93ab492ca90283e70d287
Peter Hoburg <peterHoburg@users.noreply.github.com>
2022-02-08 17:46:35 -0700

    Added venv check

--- run.sh
+++ run.sh
@@ -3,6 +3,11 @@
 sudo apt-get install python3-venv
 python3 -m venv .venv
 source .venv/bin/activate
+if [[ "$VIRTUAL_ENV" == "" ]]
+then
+  echo "Python venv failed in activate. Exiting"
+  exit 0
+fi
 python3 -m pip install ansible paramiko
 
 sudo ansible-playbook local.yml --extra-vars "main_user=$USER"
