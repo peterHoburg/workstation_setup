f4d677caa0841b5afe505db7b678422a269e3bca
Peter Hoburg <peterHoburg@users.noreply.github.com>
2022-02-08 17:41:00 -0700

    using venv

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
-python3 -m virtualenv .venv
-source ./.venv/bin/activate
+python3 -m venv .venv
+source .venv/bin/activate
 python3 -m pip install ansible paramiko
 
 sudo ansible-playbook local.yml --extra-vars "main_user=$USER"
